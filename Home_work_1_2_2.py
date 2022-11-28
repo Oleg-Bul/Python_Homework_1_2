8dc6849600f6c1543c6ef760eb0340bfcba0898c
Oleg_bul <kokseg63@gmail.com>
2022-11-28 10:03:29 +0400

    task 2 added

--- Home_work_1_2_2.py
+++ Home_work_1_2_2.py
@@ -2,3 +2,11 @@
 # выдает набор произведений чисел от 1 до N.
 # Пример:
 # - пусть N = 4, тогда [ 1, 2, 6, 24 ] (1, 1*2, 1*2*3, 1*2*3*4)
+
+n = int(input('Введите число '))
+factorialList = []
+factorial = 1
+for i in range(1, n+1):
+    factorial *= i
+    factorialList.append(factorial)
+print(factorialList)
