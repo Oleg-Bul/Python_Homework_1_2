165a1debfc6a26df3658469d65286df5b504fb54
Oleg_bul <kokseg63@gmail.com>
2022-11-28 11:12:50 +0400

    task 5 added

--- Home_work_1_2_5.py
+++ Home_work_1_2_5.py
@@ -1,1 +1,8 @@
 # Реализуйте алгоритм перемешивания списка.
+import random
+List1 = [1, 2, 3, 4, 5]
+print('Изначальный список', List1)
+for i in range(len(List1)-1, 0, -1):
+    j = random.randint(0, i + 1)
+    List1[i], List1[j] = List1[j], List1[i]
+print('Случайно перемешанный список', List1)
